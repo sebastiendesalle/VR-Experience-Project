3eff3974d9ae23ab6cefbea38122d1f924d29171
Ryan Wyters <s159260@ap.be>
2026-06-01 13:00:32 +0200

    add start menu

---
+++ Assets/Scripts/WaitingRoomIntro.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WaitingRoomIntro : MonoBehaviour
+{
+    [Header("VR Speler Lock")]
+    public Behaviour vrMovementComponent;
+
+    [Header("Sfeer (Het Donkere Effect)")]
+    public Light mainLight;
+    public float darkIntensity = 0.1f; 
+    public float normalIntensity = 1.0f;
+
+    [Header("Het Menu")]
+    public GameObject startMenuCanvas;
+
+    void Start()
+    {
+        if (vrMovementComponent != null) vrMovementComponent.enabled = false;
+
+        if (mainLight != null) mainLight.intensity = darkIntensity;
+
+        if (startMenuCanvas != null) startMenuCanvas.SetActive(true);
+    }
+
+    public void StartGameButtonClicked()
+    {
+        if (vrMovementComponent != null) vrMovementComponent.enabled = true;
+
+        if (mainLight != null) mainLight.intensity = normalIntensity;
+
+        if (startMenuCanvas != null) startMenuCanvas.SetActive(false);
+
+        Debug.Log("Waiting room is ontgrendeld. Speler kan nu bewegen!");
+    }
+}
